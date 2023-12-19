31665931843a91d53a017811302ee5b7baa44cf1
AFRIN ASHAR <98964571+afrinashar@users.noreply.github.com>
2023-12-19 16:50:46 +0530

    19 dec

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-//const API_BASE_URL = 'http://16.171.239.100:3001';
+ const API_BASE_URL = 'http://16.171.239.100:3001';
 
-const API_BASE_URL = 'http://localhost:3000';
+//const API_BASE_URL = 'http://localhost:3000';
 const api = axios.create({
   baseURL: API_BASE_URL,
 });
